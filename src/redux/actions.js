6783829b734427d98a8d3fb7494b28f6c60f8426
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-16 21:35:32 -0400

    Create the frontend  setup app

---
+++ src/redux/actions.js
@@ -0,0 +1,24 @@
+import axios from 'axios'; // You may need to install axios using npm or yarn.
+
+export const FETCH_RANDOM_GREETING_REQUEST = 'FETCH_RANDOM_GREETING_REQUEST';
+export const FETCH_RANDOM_GREETING_SUCCESS = 'FETCH_RANDOM_GREETING_SUCCESS';
+export const FETCH_RANDOM_GREETING_FAILURE = 'FETCH_RANDOM_GREETING_FAILURE';
+
+export const fetchRandomGreeting = () => async (dispatch) => {
+  dispatch({ type: FETCH_RANDOM_GREETING_REQUEST });
+
+  try {
+    // Make an API request to get a random greeting. Replace 'your_api_endpoint' with your actual API URL.
+    const response = await axios.get('your_api_endpoint');
+
+    dispatch({
+      type: FETCH_RANDOM_GREETING_SUCCESS,
+      payload: response.data, // Assuming your API returns the greeting as 'data'.
+    });
+  } catch (error) {
+    dispatch({
+      type: FETCH_RANDOM_GREETING_FAILURE,
+      error: 'Failed to fetch greeting',
+    });
+  }
+};
